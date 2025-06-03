c6eeea730bb91e7a4175b14dd4c9782de0a25102
Jason Wang <jasonwang332@gmail.com>
2025-06-03 17:55:08 +1200

    Added components folder and Hello.tsx

--- part0/new-app/src/App.tsx
+++ part0/new-app/src/App.tsx
@@ -1,20 +1,4 @@
-interface Props {
-  name: string;
-  age: number;
-}
-
-const Hello = ( {name, age}: Props ) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import Hello from "./components/Hello"
 
 const App = () => {
   const name = 'Peter'
---
+++ part0/new-app/src/components/Hello.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  name: string;
+  age: number;
+}
+
+const Hello = ( {name, age}: Props ) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+export default Hello
