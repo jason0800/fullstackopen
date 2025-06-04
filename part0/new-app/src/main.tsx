4ddc54f0bbebe65ee15c78adc195682fa6dce35a
Jason Wang <jasonwang332@gmail.com>
2025-06-04 22:00:15 +1200

    Finished 1c tutorial

--- part0/new-app/src/main.tsx
+++ part0/new-app/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+let counter = 2
+
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <App counter={counter} />
 )
