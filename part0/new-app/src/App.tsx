215ead0539c628b0c1c25cd32920ce555ec11227
Jason Wang <jasonwang332@gmail.com>
2025-06-06 19:11:55 +1200

    added unicafe project

--- part0/new-app/src/App.tsx
+++ part0/new-app/src/App.tsx
@@ -1,38 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Display = ({counter}) => <div>{counter}</div>
+const Display = ({ value }) => <p>Value: {value}</p>;
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+);
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [value, setValue] = useState(0);
+  const [total, setTotal] = useState(0);
 
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to 0, value before', counter)
-    setCounter(0)
+  const handleClick = () => {
+      console.log("Clicked")
+      const newValue = value + 1;
+      setValue(newValue);
+      setTotal(newValue);
   }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text="Plus"/>
-      <Button onClick={decreaseByOne} text="Minus"/>
-      <Button onClick={setToZero} text="Reset"/>
+      <Display value={value} />
+      <Display value={total} />
+      <Button onClick={handleClick} text="Increment" />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
