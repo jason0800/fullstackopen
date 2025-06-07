2f64795a77dab1cbecb77e9e79a11b877bff55cb
Jason Wang <jasonwang332@gmail.com>
2025-06-07 17:59:16 +1200

    Display of courseinfo set up

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -1,23 +1,33 @@
-// const Part = () => {
+const Part = ({part}) => {
+  console.log(part);
+  return <p>{part.name} {part.exercises}</p>
+}
 
-// }
-
-// const Content = () => {
-
-// }
-
-// const Header = () => {
-
-// }
-
-const Course = () => {
+const Content = ({parts}) => {
+  // console.log(parts);
   return (
-    <p>Course</p>
+    <div>
+      {parts.map(part => <Part key={part.id} part={part}/>)}
+    </div>
+  )
+}
+
+const Header = ({name}) => (
+  <h2>{name}</h2>
+)
+
+const Course = ({course}) => {
+  // console.log(course);
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+    </div>
   )
 }
 
 const App = () => {
-  const course = {
+    const course = {
     id: 1,
     name: 'Half Stack application development',
     parts: [
@@ -35,10 +45,14 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'JavaScript Basics',
+        exercises: 20,
+        id: 4
       }
     ]
   }
-
   return <Course course={course} />
 }
 
