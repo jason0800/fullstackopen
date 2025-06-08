16bcf265f8670856cd456d54b01504254d38c63a
Jason Wang <jasonwang332@gmail.com>
2025-06-08 20:58:39 +1200

    Added show all/important toggle

--- part2/notes/src/App.tsx
+++ part2/notes/src/App.tsx
@@ -4,11 +4,25 @@ import Note from "./components/Note"
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState("A new note...")
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.currentTarget)
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   const handleNoteChange = (event) => {
     console.log(event.target);
@@ -19,7 +33,7 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} content={note.content} />
         )}
       </ul>
@@ -27,6 +41,9 @@ const App = (props) => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">Save</button>
       </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        Show {showAll ? "important" : "all"}
+      </button>
     </div>
   )
 }
