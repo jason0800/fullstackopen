8d7913fb5302147ac309a3cb59026539316c5eb6
Jason Wang <jasonwang332@gmail.com>
2025-06-14 17:52:47 +1200

    finished phonebook step 9: delete person

--- part0/new-app/src/App.tsx
+++ part0/new-app/src/App.tsx
@@ -1,27 +1,11 @@
-import { useState } from 'react';
-
-const Display = ({ value }) => <p>Value: {value}</p>;
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
-);
-
 const App = () => {
-  const [value, setValue] = useState(0);
-  const [total, setTotal] = useState(0);
-
-  const handleClick = () => {
-      console.log("Clicked")
-      const newValue = value + 1;
-      setValue(newValue);
-      setTotal(newValue);
+  const handleClick = (id) => {
+      console.log(`The id is ${id}`)
   }
 
   return (
     <div>
-      <Display value={value} />
-      <Display value={total} />
-      <Button onClick={handleClick} text="Increment" />
+    <button onClick={handleClick(2)}>Click Me</button>
     </div>
   );
 };
