64a352e3bf13d6dfd1b4949f6bb9b4e183125abf
Jason Wang <jasonwang332@gmail.com>
2025-06-19 20:40:07 +1200

    added unknown endpoint middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -99,6 +99,14 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
+console.log("hello");
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
