ca1edb615aed04b50b108bfa7cc95e3dc59bd559
Jason Wang <jasonwang332@gmail.com>
2025-06-20 15:17:29 +1200

    added proxy for dev + removed cors

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(cors())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
